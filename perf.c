bc0d2efce607c5c3c13cab1f3730f97604ca9f76
tracer <tracer@tracer.org>
2018-03-19 15:06:57 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
 	}
 	else{
 		//父进程， 要通过管道读取strace的输出
-		sleep(5);
+		sleep(2);
 		printf("this is father\n");
 		close(fd[1]);	//把父进程的写管道关掉
 		/*---------初步尝试---------*/
