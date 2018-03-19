40bc0c65e452bfd2540022625e5885eda25cac26
tracer <tracer@tracer.org>
2018-03-19 14:48:43 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
 		printf("this is app:%s\n", app);
 		//strcpy(app, argv[1]);
 		for(int i = 2; i<argc; i++){
-			strcat(app, space);
+			strcat(app, 32);
 			printf("app:%s\n", app);
 			strcat(app, argv[i]);
 			printf("app:%s\n", app);
