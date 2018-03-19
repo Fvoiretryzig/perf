0d53e152d3fc7e43000978379ece59b65bbbfdc6
tracer <tracer@tracer.org>
2018-03-19 15:01:36 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -43,8 +43,8 @@ int main(int argc, char *argv[]) {
 		}
 			
 		char *fd_str="";
-		printf("fd[1]:%d\n", fd[1]);
-		sprintf(fd_str, "%d", fd[1]);	//此处fd[1]是子进程的写管道！！！尝试是不是用字符串表示！！！
+		//printf("fd[1]:%d\n", fd[1]);
+		sprintf(fd_str, "%s", fd[1]);	//此处fd[1]是子进程的写管道！！！尝试是不是用字符串表示！！！
 		printf("fd_str:%s\n", fd_str);
 		char *child_argv[ ]={"strace", "-w -c", app, "&>", fd_str, NULL}; 
 		char *child_envp[ ]={"PATH=/bin", NULL};
