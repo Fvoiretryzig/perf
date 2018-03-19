61f12f4d1538a4d78fd0ac293f39d1d90852a1be
tracer <tracer@tracer.org>
2018-03-19 20:52:23 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -35,23 +35,14 @@ int main(int argc, char *argv[]) {
 	pid_t pid = fork();
 	if(pid == 0){
 		//pid为0是子进程，子进程调用execve执行strace去读系统调用次数
-//		char *app="";
-//		app = argv[1];	
-//		for(int i = 2; i<argc; i++){
-//			sprintf(app, "%s %s",app ,argv[i]);
-//			printf("this is app:%s\n", app);
-//		}
 		close(fd[0]);
-		//char fd_str[11];
-		//sprintf(fd_str, "%d", fd[1]);	//此处fd[1]是子进程的写管道！！！尝试是不是用字符串表示！！！
-		//printf("fd_str:%s\n", fd_str);
 		
 		char another_argv[100][100]; 
 		for(int i = 0; i<argc; i++)
 			strcpy(another_argv[i], argv[i]);
-		strcpy(argv[0], "strace");
-		strcpy(argv[1], "-w");
-		strcpy(argv[2], "-c");			
+		strcpy(argv[0], "strace"); strcpy(argv[1], "-w"); strcpy(argv[2], "-c");
+		strcpy(argv[3], "hahaha");
+		printf("argv[3]:%s\n", argv[3]);			
 		for(int i = 1; i<argc; i++){
 			printf("0:%s 1:%s 2:%s\n",argv[0], argv[1], argv[2]);
 			strcpy(argv[i+2], another_argv[i]);
