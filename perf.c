8dda01e7ed13560f026129a3f8f1fc8a6a992b1b
tracer <tracer@tracer.org>
2018-03-19 21:43:17 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -40,8 +40,6 @@ int main(int argc, char *argv[]) {
 		char child_argv[100][100]={"strace","-w","-c"};
 		strcpy(child_argv[3], argv[1]);
 		printf("3:%s\n", child_argv[3]);
-		child_argv[3] = argv[1];
-		child_argv[4] = argv[2];
 		char **temp = child_argv;
 		dup2(fd[1],2);	//把strace的输出连接到子进程的写管道
 		execvp("strace", temp);
