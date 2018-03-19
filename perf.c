7d7a6056ff38ea6c33a237360c891c2a2b85a064
tracer <tracer@tracer.org>
2018-03-19 15:21:13 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -37,6 +37,8 @@ int main(int argc, char *argv[]) {
 		//pid为0是子进程，子进程调用execve执行strace去读系统调用次数
 		char *app="";
 		app = argv[1];
+		close(fd[0]);
+		close(1);
 		for(int i = 2; i<argc; i++){
 			sprintf(app, "%s %s",app ,argv[i]);
 			printf("this is app:%s\n", app);
