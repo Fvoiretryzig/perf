9e7ab3e60e142443284ddde3ccc05226d3a38b27
tracer <tracer@tracer.org>
2018-03-19 14:24:11 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -1,3 +1,62 @@
-int main(int argc, char *argv[]) {
-  return 0;
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<stdlib.h>
+
+struct systemcall
+{
+	float per_time;
+	char *name;
+};
+struct systemcall syscall[300];
+void do_str(char *s)
+{
+	
+}
+int main(int argc, char *argv[]) 
+{
+	/*--------读取命令行参数--------*/
+	for (int i = 0; i < argc; i++) {
+		assert(argv[i]); // specification
+	    printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	assert(!argv[argc]); // specification	
+	if(argc == 1){
+		printf("Usage: stat command [argument]")
+		exit(1);
+	}
+	/*--------连接管道，创建子进程--------*/
+	int fd[2] = {0, 0};
+	if(pipe(fd) != 0){
+		printf("pipe error\n");
+		exit(1);
+	}
+	pid_t pid = fork();
+	if(pid == 0){
+		//pid为0是子进程，子进程调用execve执行strace去读系统调用次数
+		char *app;
+		strcpy(app, argv[1]);
+		for(int i = 1; i<argc; i++)
+			strcat(app, argv[i]);
+		char *fd_str;
+		sprintf(fd_str, "%d", fd[1]);	//此处fd[1]是子进程的写管道！！！尝试是不是用字符串表示！！！
+		char *child_argv[ ]={"strace", "-w -c", app, "&>", fd_str, NULL}; 
+		char *child_envp[ ]={"PATH=/bin", NULL};
+		execve("/bin/strace", child_argv, child_envp);
+	}
+	else{
+		//父进程， 要通过管道读取strace的输出
+		close(fd[1]);	//把父进程的写管道关掉
+		/*---------初步尝试---------*/
+		char buf[1024];
+		while(1){
+			ssize_t len = read(fd[0], buf, 1024);
+			if(len > 0)
+				printf("read:\n%s\n", buf);
+			else exit(0);
+		}
+		
+	}
+	return 0;
 }
