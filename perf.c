a146c1adfd3f64c513ed217b960cd3baac2cd88c
tracer <tracer@tracer.org>
2018-03-20 14:17:57 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -40,9 +40,7 @@ int main(int argc, char *argv[]) {
 	if(pid == 0){
 		//pid为0是子进程，子进程调用execve执行strace去读系统调用次数
 		char *child_argv[100]={"strace","-w","-c", "", "","","","","","","","","",""};
-		//printf("%s\n", child_argv[3]);
 		char **temp = argv;
-		//printf("temp:%s\n", temp[1]);
 		for(int i = 1; i<argc; i++){
 			child_argv[i+2] = (char*)temp[i];
 			printf("child_argv[i+2]:%s\n", child_argv[i+2]);
@@ -50,12 +48,7 @@ int main(int argc, char *argv[]) {
 		printf("argc:%d\n", argc);
 		child_argv[argc+2] = NULL;
 		close(fd[0]);
-		//child_argv[5] = NULL;
-		//printf("%s\n",child_argv[5]);
-		//int open_fd = open("mystatus.txt",O_CREAT | O_RDWR | O_TRUNC,S_IRUSR | S_IWUSR);
 		dup2(fd[1],2);	//把strace的输出连接到子进程的写管道
-		//dup2(fd[1],open_fd);
-		//dup2(open_fd,2);
 		execvp("strace", child_argv);
 		//close(fd[1]);
 		exit(0);
@@ -69,9 +62,10 @@ int main(int argc, char *argv[]) {
 		int open_fd = open("mystatus.txt",O_CREAT | O_RDWR | O_TRUNC,S_IRUSR | S_IWUSR);
 		dup2(open_fd,fd[1]);
 		char *buf;
-		while(1){
+		//while(1)
+		while((read(fd[0], &buf, 1)>0){
 			printf("this is father\n");
-			//dup2(1,fd[0]);
+			printf("%c\n", buf);
 			ssize_t len = read(fd[0], buf, 1024);
 			printf("%d\n", len);
 			if(len > 0)
