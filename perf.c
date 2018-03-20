a1c5366083dd6d3a2527d5f1ebd27b8c01f78355
tracer <tracer@tracer.org>
2018-03-21 01:23:58 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -97,6 +97,7 @@ int main(int argc, char *argv[]) {
 						printf("error!");
 					}	
 					else{
+						printf("temp:%s\n", temp);
 						p_name = regexec(&reg_name,temp,1,pm_name,0);
 						char *r_name = substr(temp,pm_name[0].rm_so,pm_name[0].rm_eo);
 						if(!strcmp(r_name , "---------------"))
