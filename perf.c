d4289879cc4a2671301193a6fb69572dc22993c3
tracer <tracer@tracer.org>
2018-03-20 21:48:31 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -59,6 +59,7 @@ int main(int argc, char *argv[]) {
 		ssize_t len = read(fd[0], buf, sizeof(buf));
 		printf("len:%d\n", len);	
 		char *temp = strtok(buf[0], " ");
+		temp = strtok(temp+strlen(temp)+1, "\n");
 		for(int i = 0; i<300; i++){
 			temp = strtok(temp, "\n");
 			printf("temp%d:%s\n ", i, temp);			
