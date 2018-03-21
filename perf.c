97052c80b2d0cae274ccdd1a2ae8d0b8f46bff75
tracer <tracer@tracer.org>
2018-03-21 09:26:29 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- perf.c
+++ perf.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[]) {
 			}	
 			else{
 				if(i >= 2){
-					char *pattern_name = "[A-Za-z]+([0-9]+)?";	//用来匹配名字的
+					char *pattern_name = "[A-Za-z,_]+([0-9]+)?";	//用来匹配名字的
 					regex_t reg_name;
 					int p_name = regcomp(&reg_name,pattern_name, REG_EXTENDED);
 					
